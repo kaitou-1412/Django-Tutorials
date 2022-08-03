64d730921ba5f1dcbf5a47fc9906c53cda0868e3
Ruban Sahoo <rubansahoo1@gmail.com>
2022-08-03 23:12:19 +0530

    Review model added - GenericAPIView and Mixins

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,21 +1,29 @@
 from rest_framework import serializers
-from watchlist_app.models import WatchList, StreamPlatform
+from watchlist_app.models import WatchList, StreamPlatform, Review
 
 
-class WatchListSerializer(serializers.HyperlinkedModelSerializer):
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class WatchListSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = WatchList
         fields = "__all__"
 
 
-class StreamPlatformSerializer(serializers.HyperlinkedModelSerializer):
-    # watchlist = WatchListSerializer(many=True, read_only=True)
-    watchlist = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='watchlist-detail'
-    )
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+    # watchlist = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only=True,
+    #     view_name='watchlist-detail'
+    # )
 
     class Meta:
         model = StreamPlatform
--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 # from .views import movie_list, movie_details
-from .views import WatchListAV, WatchDetailAV, StreamPlatformListAV, StreamPlatformDetailAV
+from .views import ReviewList, ReviewDetail, WatchListAV, WatchDetailAV, StreamPlatformListAV, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watchlist-list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='watchlist-detail'),
     path('stream/list/', StreamPlatformListAV.as_view(), name='streamplatform-list'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
+
+    path('review/', ReviewList.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
 ]
