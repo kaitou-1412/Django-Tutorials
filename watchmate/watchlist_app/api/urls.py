0610779b37cf78065472187575db4c3412efff65
Ruban Sahoo <rubansahoo1@gmail.com>
2022-08-04 07:29:58 +0530

    Overwrite queryset

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 # from .views import movie_list, movie_details
-from .views import ReviewList, ReviewDetail, WatchListAV, WatchDetailAV, StreamPlatformListAV, StreamPlatformDetailAV
+from .views import ReviewCreate, ReviewList, ReviewDetail, WatchListAV, WatchDetailAV, StreamPlatformListAV, StreamPlatformDetailAV
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='watchlist-list'),
@@ -8,6 +8,10 @@ urlpatterns = [
     path('stream/list/', StreamPlatformListAV.as_view(), name='streamplatform-list'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='streamplatform-detail'),
 
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+    # path('review/', ReviewList.as_view(), name='review-list'),
+    # path('review/<int:pk>', ReviewDetail.as_view(), name='review-detail'),
+
+    path('stream/<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]
