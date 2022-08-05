096b02506a74f37df1821175b93c2544ec6826bc
Ruban Sahoo <rubansahoo1@gmail.com>
2022-08-05 08:18:32 +0530

    Permissions

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import status, generics, viewsets
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from watchlist_app.models import WatchList, StreamPlatform, Review
+from .permissions import AdminOrReadOnly, ReviewUser0rReadOnly
 from .serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 
 
@@ -21,11 +23,15 @@ class ReviewCreate(generics.CreateAPIView):
         review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie!")
+        watchlist.avg_rating = (watchlist.avg_rating * watchlist.number_rating + serializer.validated_data['rating'])/(watchlist.number_rating + 1)
+        watchlist.number_rating += 1
+        watchlist.save()
         serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         pk = self.kwargs['pk']
@@ -35,6 +41,7 @@ class ReviewList(generics.ListAPIView):
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [ReviewUser0rReadOnly]
 
 # class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
 #     queryset = Review.objects.all()
